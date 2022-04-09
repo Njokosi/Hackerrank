8505acb09030d61329c8a49dd6747705e53ee89e
Njokosi Kawunju <n.kawunju@zuno-ai.com>
2022-04-09 14:23:31 +0300

    :zap: Solve two sum challenge

--- Leetcode/1.Two Sum.py
+++ Leetcode/1.Two Sum.py
@@ -21,3 +21,32 @@ Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
 """
+
+
+class Solution(object):
+
+    def two_sum(self, nums, target):
+        
+        nums_index = [(nums[i], i) for i in range(len(nums))]
+        nums_index.sort()
+        
+        i, j = 0, len(nums_index) - 1
+        while i < len(nums_index):
+            current_sum = nums_index[i][0] + nums_index[j][0]
+            
+            if current_sum == target:
+                return nums_index[i][1], nums_index[j][1]
+            
+            elif current_sum < target:
+                i += 1
+                
+            else:
+                j -= 1
+                
+            
+
+nums = list(map(int, input("Enter list separated by space: ").rstrip().split()))
+target = int(input("Enter your target: "))
+
+s = Solution()
+print(s.two_sum(nums, target))
